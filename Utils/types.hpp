85ce5f706e4dcc45a97a4f40e3ba32646d905d5b
Vincent <vincent1.son@epitech.eu>
2021-09-01 22:26:04 +0200

    added dotProduct on Vec3

--- Utils/types.hpp
+++ Utils/types.hpp
@@ -19,6 +19,10 @@ class Vec3 {
             this->_y = i;
             this->_z = i;
         };
+        static T dotProduct(Vec3<T> v1, Vec3<T> v2) {
+            return (v1._x * v2._x) + (v1._y * v2._y) + (v1._z * v2._z);
+        }
+        // ! operator* overloads may need some refactor, I don't really like how they are implemented atm.
         Vec3<T> operator*(const Vec3<T> &mult) {
             T x = (this->_y * mult._z) - (this->_z * mult._y);
             T y = (this->_x * mult._z) - (this->_z * mult._x);
@@ -26,16 +30,18 @@ class Vec3 {
             return Vec3<T>(x, y, z);
         };
         Vec3<T> operator*(const T &mult) {
-            this->_x *= mult;
-            this->_y *= mult;
-            this->_z *= mult;
-        }
-        friend std::ostream& operator<<(std::ostream &os, const Vec3<T> v) {
-            os << v._x << ", " << v._y << ", " << v._z;
-            return os;
+            T x = this->_x * mult;
+            T y = this->_y * mult;
+            T z = this->_z * mult;
+            return Vec3<T>(x, y, z);
         }
         ~Vec3() {
 
         };
         T _x, _y, _z;
+    private:
+        friend std::ostream& operator<<(std::ostream &os, const Vec3<T> v) {
+            os << v._x << ", " << v._y << ", " << v._z;
+            return os;
+        }
 };
