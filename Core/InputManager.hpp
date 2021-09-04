c8008533952b4decaec8fefed36ca1ae4a96aece
Vincent <vincent1.son@epitech.eu>
2021-09-04 16:39:08 +0200

    added InputManager for Windows

---
+++ Core/InputManager.hpp
@@ -0,0 +1,26 @@
+#include "./EntityManager.hpp"
+
+// #if WIN32
+//     #include <windows.h>
+// #endif
+
+#include <Windows.h>
+
+class InputManager {
+    public:
+        InputManager() {
+        };
+        void update() {
+            for (int i = 0; i < 256; i++) {
+                this->key_state[i] = (GetAsyncKeyState(i) & 0x8000) ? 1 : 0;
+            }
+        }
+        bool isKeyPressed(int x) {
+            return key_state[x];
+        }
+        ~InputManager() {
+
+        };
+    private:
+        bool key_state[256] = {};
+};
